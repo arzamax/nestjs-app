4e76657f3127caf677d9fb4ff8130218505392b9
Maxim Arzamastsev <arzamax@gmail.com>
2019-10-04 21:50:59 +0400

    Added sign up logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TasksModule } from './tasks/tasks.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { typeOrmConfig } from '../config/typeorm.config'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     TasksModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,32 @@
+import { EntityRepository, Repository } from 'typeorm'
+import { ConflictException, InternalServerErrorException } from '@nestjs/common'
+import * as bcrypt from 'bcrypt'
+
+import { User } from './user.entity'
+import { AuthCredentialsDto } from './dto/auth-credentials.dto'
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto
+    const user = new User()
+
+    user.username = username
+    user.salt = await bcrypt.genSalt()
+    user.password = await UserRepository.hashPassword(password, user.salt)
+
+    try {
+      await user.save()
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists')
+      } else {
+        throw new InternalServerErrorException()
+      }
+    }
+  }
+
+  static async hashPassword(password: string, salt: string): Promise<string> {
+    return await bcrypt.hash(password, salt)
+  }
+}
